90080abdebb4162ccdab8cf51cf41df083ec974d
up240648 <up240648@alumnos.upa.edu.mx>
2025-02-26 15:10:36 -0600

    01_Dayexercises

---
+++ 01_Dayexercises.py
@@ -0,0 +1,14 @@
+2
+print(4 + 5)
+print(8 - 2)
+print(6 * 2)
+print(8 / 4)
+print(6 ** 3)
+print(5 % 3)
+print(5 // 9)
+
+3
+print('Name: Saul Obed ')
+print('Family name: Acevedo Alonso')
+print('Contry: Mexico')
+print('I am having a blast learning python')
