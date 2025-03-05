65f02d2de7da31b4daa59c28b8528b8eed33c63e
up240648 <up240648@alumnos.upa.edu.mx>
2025-03-05 15:31:03 -0600

    Add files via upload

---
+++ 04_Dayexercises.py
@@ -0,0 +1,58 @@
+# Ejercicio 1
+
+No1 = 'Thirty'
+No2 = ' Days'
+No3 = 'Of'
+No4 = 'Python'
+space = ' '
+full = No1 +space+ No2 +space+ No3 +space+ No4
+print(full)
+print(len(full))
+
+# Ejercicio 2
+
+no1 = 'Coding'
+no2 = 'For'
+no3 = 'All'
+space = ' '
+Full = no1 + space + no2 + space + no3 
+print(Full)
+print(len(Full))
+
+# Ejercicio 3
+
+company = 'Coding For All'
+
+# Ejercicio 4
+
+print(company)
+
+# Ejercicio 5
+
+print(len(company))
+
+# Ejercicio 6
+
+print(company.upper())
+# Ejercicio 7
+
+print(company.lower())
+
+# Ejercicio 8
+
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+
+# Ejercicio 9
+
+print(company[0:6])
+
+# Ejercicio 10
+
+print(company.index('Coding'))
+
+# Ejercicio 11
+
+
+
