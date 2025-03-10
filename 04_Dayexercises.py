46676984628cb890ecf22cd41f2128c3aaa08196
up240648 <up240648@alumnos.upa.edu.mx>
2025-03-10 15:32:38 -0600

    Update 04_Dayexercises.py

--- 04_Dayexercises.py
+++ 04_Dayexercises.py
@@ -54,5 +54,68 @@ print(company.index('Coding'))
 
 # Ejercicio 11
 
+print(company.replace('Coding','Pytohn'))
+
+# Ejercicio 12
+
+oracion = 'Pytohn For Everyone' 
+print(oracion.replace('Everyone','All'))
+
+#Ejercicio 13
+
+print(company.split())
+
+# Ejercicio 14
+
+empresas = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+print(empresas.split())
+
+# Ejercicion 15
+
+print(company[0])
+
+# Ejercicio 16
+
+print(company[13])
+
+# Ejercicio 17
+
+print(company[10])
+
+# Ejercicio 18
+
+Frase = 'Python For Everyone'
+acronimo = "".join(word[0] for word in Frase.split())
+print(acronimo)
+
+# Ejercicio 19 
+
+Frase2 = 'Coding For All'
+acronimo2 = "".join(word[0] for word in Frase2.split())
+print(acronimo2)
+
+# Ejercicio 20
+
+print(company.index('C'))
+
+# Ejercicio 21
+
+print(company.index('F'))
+
+# Ejercicio 22
+
+print(company.rfind('l'))
+
+# Ejercicio 23
+
+O = 'You cannot end a sentence with because because because is a conjunction'
+print(O.index('because'))
+
+# Ejercicio 24
+
+print(O.rindex('because'))
+
+# Ejercicio 25
+
 
 
