9372299e6e7b848c718af9eb4a3a00e8089aefed
up240648 <up240648@alumnos.upa.edu.mx>
2025-03-24 15:16:29 -0600

    Add files via upload

---
+++ 07_Dayexercise1.py
@@ -0,0 +1,21 @@
+ # Ejercicio 1
+it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+
+# Ejercicio 2
+print(len(it_companies))
+
+# Ejercicio 3
+it_companies.add('Twitter')
+print(it_companies)
+
+# Ejercicio 4
+it_companies.update(['X', 'Steam', 'Instagram'])
+print(it_companies)
+
+# Ejercicio 5
+it_companies.remove('Twitter')
+print(it_companies)
+
+# Ejercicio 6 
+it_companies.discard('OLA')
+print(it_companies)
